d2a4fbb2aa19b4a539eba09b5b5dd84a24be0ef6
Alexey Petrushin <alexey.petrushin@gmail.com>
2013-09-24 05:54:41 +0400

    Add support for CoffeeScript

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 var through = require('through')
   , jsp = require('uglify-js').parser
   , pro = require('uglify-js').uglify
-  , path = require('path')
 
 module.exports = uglifyify
 function uglifyify(file) {
   var buffer = ''
 
-  if (path.extname(file) !== '.js') return through()
+  if (!/\.js$|\.coffee$/.test(file)) return through()
 
   return through(function write(chunk) {
     buffer += chunk
